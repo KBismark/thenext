062e6b943e765d8540cf84b827f1689091e8dfc8
KBismark <bismarkkwabenayamoah@gmail.com>
2023-04-24 17:30:46 +0000

    Compiler works

--- src/module/home/index.js
+++ src/module/home/index.js
@@ -1,16 +1,14 @@
 
 
-!function(){
-    JSHON.pathname = "/module/home/index.js";
-    JSHON.include("/module/home/app.js");
-    JSHON.onload = function(){
-        JSHON.ui.createApp(
-            "/",
-            JSHON.import.from("/module/home/app.js").App.getInstanceRef(),
-            function(){},
-            typeof(window)=="undefined"?null:document.getElementById("page")
-        )
-    }
-    
-
-}();
+!(function () {
+  Breaker.pathname = "/module/home/index.js";
+  Breaker.include("/module/home/app.js");
+  Breaker.onload = function () {
+    Breaker.ui.createApp(
+      "/",
+      Breaker.import.from("/module/home/app.js").App.getInstanceRef(),
+      function () {},
+      typeof window == "undefined" ? null : document.getElementById("page")
+    );
+  };
+})();
--- src/ssr/module/home/index.js
+++ src/ssr/module/home/index.js
@@ -1,18 +1,17 @@
 module.exports = function(JSHON){
 
 
-!function(){
-    JSHON.pathname = "/module/home/index.js";
-    JSHON.include("/module/home/app.js");
-    JSHON.onload = function(){
-        JSHON.ui.createApp(
-            "/",
-            JSHON.import.from("/module/home/app.js").App.getInstanceRef(),
-            function(){},
-            typeof(window)=="undefined"?null:document.getElementById("page")
-        )
-    }
-    
+!(function () {
+  Breaker.pathname = "/module/home/index.js";
+  Breaker.include("/module/home/app.js");
+  Breaker.onload = function () {
+    Breaker.ui.createApp(
+      "/",
+      Breaker.import.from("/module/home/app.js").App.getInstanceRef(),
+      function () {},
+      typeof window == "undefined" ? null : document.getElementById("page")
+    );
+  };
+})();
 
-}();
 }
